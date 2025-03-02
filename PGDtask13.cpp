ec2cbcdfee6469fbfdf14a723f7f25546c9c6f5d
AliSaleem7542 <alisaleem7542@gmail.com>
2025-03-02 23:06:58 +0500

    My first commit

---
+++ PGDtask13.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main() {        
+
+int a,sum;
+cout<<"Enter integer: ";
+cin>>a;
+sum=sum+a;
+cout<<"Enter integer: ";
+cin>>a;
+sum=sum+a;
+cout<<"Enter integer: ";
+cin>>a;
+sum=sum+a;
+cout<<"Enter integer: ";
+cin>>a;
+sum=sum+a;
+cout<<"Enter integer: ";
+cin>>a;
+sum=sum+a;
+cout<<"sum  :"<<sum;
+
+}
